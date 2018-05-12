7fbd5debb79079f1bccade8285b357fb0010b00d
15301005 <15301005@bjtu.edu.cn>
2018-05-12 10:09:43 +0800

    创建菜谱/菜谱详情页面

---
+++ app/src/main/java/com/example/morphtin/dishes/DishDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.morphtin.dishes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DishDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish_detail);
+    }
+}
