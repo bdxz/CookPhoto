bda34885c6808935358fa216aa8c9fddf4ff904e
zzk001 <15301111@bjtu.edu.cn>
2018-06-18 16:12:57 +0800

    更改message

---
+++ app/src/main/java/com/example/morphtin/dishes/ui/activity/MessageActivity.java
@@ -0,0 +1,16 @@
+package com.example.morphtin.dishes.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.morphtin.dishes.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_message);
+
+    }
+}
